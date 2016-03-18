efd807649b6e5899dcbed87019e646955eaa3106
scanalyzer <dan@mcdanielar.com>
2016-03-17 23:47:22 -0700

    To-Dos Updated

--- TODO.cpp
+++ TODO.cpp
@@ -1,15 +1,12 @@
 /*
 Complete the following:
- Create board.h
- Create board.cpp
-
-
-
-
-
-
-
-
-
-
+ getMoves for each piece
+ validate move against possible moves
+ verify user friendly board
+ readFile and saveFile
+ errors
+ comment all code
+ promote
+ castle // ingore check condition
+ enpassant
 */

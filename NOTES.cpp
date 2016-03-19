bd37adb97493d2460f3d8cb59db9e36a4e46a7cc
scanalyzer <dan@mcdanielar.com>
2016-03-19 10:40:11 -0700

    Added notes on circular dependencies, renamed todo to notes.

---
+++ NOTES.cpp
@@ -0,0 +1,25 @@
+/*
+Complete the following:
+ getMoves for each piece
+ validate move against possible moves
+ verify user friendly board
+ readFile and saveFile
+ errors
+ comment all code
+ promote
+ castle // ingore check condition
+ enpassant
+ 
+ 
+Notes:
+ Circular Dependencies
+ 
+ * Use forward declarations where possible.
+ 
+ * Move any header includes out of a header file and into the corresponding
+   cpp file if they are only needed by the cpp file. Easiest way to enforce
+   this is to make the #include "myclass.h" the first include in myclass.cpp.
+ 
+ * Introducing interfaces at the point of interaction between separate classes
+   can help reduce dependencies.
+*/
